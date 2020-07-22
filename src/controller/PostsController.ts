69c9f3d4fb5aa098a046f7a57708161cfbf5b774
Osman Rodrigues <55052153+OsmanRodrigues@users.noreply.github.com>
2020-07-22 16:55:08 -0300

    Revert "implementação do endpoit getpostbyid"

--- src/controller/PostsController.ts
+++ src/controller/PostsController.ts
@@ -4,29 +4,18 @@ import {PostsDatabase} from '../data/PostsDatabase';
 
 export class PostsController {
 
-  async createPost(req: Request, res: Response): Promise<void>{
-    try{
-      const body = req.body;
-      await new PostsBusiness().createPost(
-        body.img_url, body.creator_id, body.type, body.create_at, body.description
-      );
-      
-      res.send({message: `Post created successfully!`}).status(200);
+  async createPost(req: Request, res: Response){
+      try{
+        const body = req.body;
+        await new PostsBusiness().createPost(
+          body.img_url, body.type, body.create_at, body.description
+        );
+        
+        res.send({message: `Post created successfully!`}).status(200);
 
-      await new PostsDatabase().destroyConnection();
-    }catch(e){
-        res.status(400).send({error: e.message});
-    }
-  };
-
-  async getPostById(req: Request, res: Response): Promise<void>{
-    try{
-      const postId = req.params.id as string;
-      const response = await new PostsBusiness().getPostById(postId);
-      //TODO: converter a data de moment para formato DD/MM/AAAA
-      res.send({post: response}).status(200);
-    }catch(e){
-      res.status(400).send({error: e.message});
-    };
-  };
+        await new PostsDatabase().destroyConnection();
+      }catch(e){
+          res.status(400).send({error: e.message});
+      }
+  }
 };
--- src/router/PostsRouter.ts
+++ src/router/PostsRouter.ts
@@ -4,5 +4,4 @@ import { PostsController } from "../controller/PostsController";
 export const postsRouter = express.Router();
 
 postsRouter.post("/create", new PostsController().createPost);
-postsRouter.get("/:id", new PostsController().getPostById);
 
