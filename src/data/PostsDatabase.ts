6ee86c32f920c7d7bffd75e61d1ccdb9737059c5
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-07-22 16:49:32 -0300

    implementação do endpoit getpostbyid

--- src/data/PostsDatabase.ts
+++ src/data/PostsDatabase.ts
@@ -4,7 +4,7 @@ import {createPostInput} from '../model/PostsModels';
 export class PostsDatabase extends BaseDatabase{
   private tableName: string = process.env.POSTS_DB_NAME;
 
-  public async createPost(input: createPostInput){
+  public async createPost(input: createPostInput): Promise<void>{
     try{
       //TODO: receber id do criador do post
       await this.getConnection()
@@ -13,7 +13,8 @@ export class PostsDatabase extends BaseDatabase{
         img_url: input.img_url,
         description: input.description,
         create_at: input.create_at,
-        type: input.type
+        type: input.type,
+        creator_id: input.creator_id
       })
       .into(this.tableName);
 
@@ -22,7 +23,16 @@ export class PostsDatabase extends BaseDatabase{
     };
   };
   
-  public async getPostById(){
+  public async getPostById(id:string): Promise<any>{
+    try{
+      const response = await this.getConnection()
+      .select('*')
+      .from(this.tableName)
+      .where({id});
 
+      return response[0]
+    }catch(e){
+      throw {message: e.sqlMessage || e.message}
+    };
   };
 };
--- src/model/PostsModels.ts
+++ src/model/PostsModels.ts
@@ -3,6 +3,7 @@ interface createPostInput{
   img_url: string,
   type?:string,
   create_at?: string,
-  description?:string
+  description?:string,
+  creator_id: string
 };
 export {createPostInput};
