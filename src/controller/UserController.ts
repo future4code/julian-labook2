559f4e0b8f8044bff3614be71aa7afc437d73c36
WellDMLT <wellington.acardoso18@gmail.com>
2020-07-23 00:19:30 -0300

    Refatoração do código

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
+
 export class UserController {
 
     async signup(req: Request, res: Response) {
@@ -8,35 +9,11 @@ export class UserController {
 
             const name = req.body.name;
             const email = req.body.email;
-            await userBusiness.signup(name, email);
+            const password = req.body.password
+
+            await userBusiness.signup(name, email, password);
             res.status(200).send({ message: "Usuário criado com sucesso" });
 
-        } catch (err) {
-            res.status(400).send({ error: err.message });
-        }
-
-    }
-
-    async approve(req: Request, res: Response) {
-        const userBusiness: UserBusiness = new UserBusiness();
-        try {
-            const id = req.body.id
-
-            await userBusiness.approve(id);
-
-            res.status(200).send({ message: "Usuário aprovado" });
-
-        } catch (err) {
-            res.status(400).send({ error: err.message });
-        }
-    }
-
-    async getUserById(req: Request, res: Response) {
-        const userBusiness: UserBusiness = new UserBusiness();
-        try {
-            const id = req.params.id;
-            const result = await userBusiness.getUserById(id);
-            res.status(200).send(result);
         } catch (err) {
             res.status(400).send({ error: err.message });
         }
