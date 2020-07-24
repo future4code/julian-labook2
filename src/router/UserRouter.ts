4bb83d81777b74e34987542d40b7dad9ced95943
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-07-24 16:24:08 -0300

    implementação dos endpoints create e delete friendship

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -4,11 +4,12 @@ import { UserController } from "../controller/UserController";
 
 export const userRouter = express.Router();
 
-const userController = new UserController
-
-
+const userController = new UserController;
 
 userRouter.post("/signup", userController.signup);
 
-userRouter.post("/login", userController.login)
+userRouter.post("/login", userController.login);
 
+userRouter.post('/friendship', userController.makeFriendship);
+
+userRouter.delete('/friendship', userController.deleteFriendship);
